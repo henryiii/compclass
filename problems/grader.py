a73cb063f41b10bebb8bd44a82d1dbec78839b3f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-09-15 17:03:34 +0200

    Adding updates for week 1

--- problems/grader.py
+++ problems/grader.py
@@ -6,6 +6,10 @@ from contextlib import redirect_stdout
 from io import StringIO
 import copy
 
+import sys
+if '.' not in sys.path:
+    sys.path.append('.')
+
 class Score:
     '''
     curscore: the current score (between 0 and maxscore)
@@ -65,7 +69,19 @@ class Grader(cli.Application):
     hide = cli.Flag('--hide', help="Hide the output (usually combined with --full)")
     verbose = cli.Flag('--verbose', help="Add a printout for each success, too")
 
-    def main(self, infile: cli.ExistingFile):
+    def main(self, *infiles: cli.ExistingFile):
+        for infile in infiles:
+            try:
+                self.each(infile)
+            except:
+                if len(infiles) > 1:
+                    print("Failed:", infile)
+                else:
+                    raise
+
+    def each(self, infile: cli.ExistingFile):
+        self.total_score = Score.empty()
+
         base = 'ipynb.fs.' + ('full' if self.full else 'defs') + '.' + infile.stem
         if self.hide:
             tmp = StringIO()
@@ -74,8 +90,16 @@ class Grader(cli.Application):
         else:
             self.mod = import_module(base)
 
-        (colors.info | colors.bold).print(self.mod.EID, ":", self.mod.NAME)
-        self.total_score = Score.empty()
+        (colors.info | colors.bold).print(
+                getattr(self.mod, 'EID', 'None'), ":",
+                getattr(self.mod, 'NAME', 'None'))
+
+        if not hasattr(self.mod, 'EID'):
+            self.penalty('EID missing!', 1)
+
+        if not hasattr(self.mod, 'NAME'):
+            self.penalty('NAME missing!', 1)
+
         for i in range(10):
             if hasattr(self, f'process{i}'):
                 self.score = Score.empty()
@@ -97,16 +121,23 @@ class Grader(cli.Application):
             colors.success.print('Scoring problem', self.score.times, 'with', factor, 'points')
 
     def failure(self, error, *, score=0, msg=None, factor=1):
-        self.score += Score(score)
-        colors.fatal.print(f'  Test case {self.score.times}: {error}')
+        self.score += Score(score) * factor
+        colors.fatal.print(f'  Test case {self.score.times} ({score}/{factor}): {error}')
         if msg:
             colors.fatal.print('    ' + msg)
+
+    def penalty(self, msg, factor=1):
+        self.total_score.curscore -= factor
+        colors.fatal.print(f'  {msg}: {-factor}')
+
 
     # Comparison methods
     # All methods should either pass on msg and factor
     # as keyword only arguments
 
     def compare_in(self, item, inside, *, msg=None, factor=1):
+        if item is None:
+            return self.failure(f'"{inside}" not in None', msg=msg, factor=factor)
         if inside in item:
             return self.success(msg=msg, factor=factor)
         else:
@@ -116,7 +147,7 @@ class Grader(cli.Application):
         if item == other:
             return self.success(msg=msg, factor=factor)
         else:
-            return self.failure('"{item}" != "{other}"', msg=msg, factor=factor)
+            return self.failure(f'"{item}" != "{other}"', msg=msg, factor=factor)
 
     def valid(self, item, *, msg=None, factor=1):
         if item:
@@ -129,9 +160,9 @@ class Grader(cli.Application):
         try:
             function(*args, **kargs)
             return self.failure(f'  Expected {function.__name__} to raise {exception.__name__} and it didn\'t raise anything', msg=msg, factor=factor)
-        except exception:
+        except exception as e:
             return self.success(factor=factor, msg=msg)
-        return self.failure(f'  Expected {function.__name__} to raise {exception.__name__} and it raised something else (half credit)', score=.5, msg=msg, factor=factor)
+        return self.failure(f'  Expected {function.__name__} to raise {exception} and it raised something else (half credit)', score=.5, msg=msg, factor=factor)
 
     def nullify(self, *, msg=None):
         'Clear the score with optional message'
